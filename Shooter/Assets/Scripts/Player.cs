fa34c02be6268a5e49536e7a9d071d31c6da6cc4
grahag3 <grahag3@rpi.edu>
2019-03-29 14:38:56 -0400

    Impemented basic player movement

---
+++ Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float rotspeed;
+    public float speed;
+    public Vector3 direction;
+
+
+
+    private float rotation;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotdirection = (Input.GetAxis("Horizontal"));
+        rotation = (rotdirection* rotspeed);
+
+        transform.Rotate(0, 0, -rotation);
+
+        direction = transform.up;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(direction * speed);
+        }
+    }
+}

64b76f26fb2368fa29ee93e37cdab6315a909ac4
grahag3 <grahag3@rpi.edu>
2019-04-01 01:14:14 -0400

    Started implementation of asteroid spawning

--- Shooter/Assets/Scripts/Player.cs
+++ Shooter/Assets/Scripts/Player.cs
@@ -8,7 +8,11 @@ public class Player : MonoBehaviour
     public float speed;
     public Vector3 direction;
 
+    private float min_height = -4.30f;
+    private float max_height = 4.30f;
 
+    private float min_width = -8.4f;
+    private float max_width = 8.4f;
 
     private float rotation;
     
@@ -23,7 +27,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         float rotdirection = (Input.GetAxis("Horizontal"));
-        rotation = (rotdirection* rotspeed);
+        rotation = (rotdirection * rotspeed);
 
         transform.Rotate(0, 0, -rotation);
 
@@ -32,6 +36,31 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             GetComponent<Rigidbody2D>().AddForce(direction * speed);
+
+            
         }
+
+        if ((transform.position.y) < min_height)
+        {
+            transform.position = new Vector3(transform.position.x, min_height, 0);
+        }
+
+        if ((transform.position.y) > max_height)
+        {
+            transform.position = new Vector3(transform.position.x, max_height, 0);
+        }
+
+        if ((transform.position.x) < min_width)
+        {
+            transform.position = new Vector3(min_width, transform.position.y, 0);
+        }
+
+        if ((transform.position.x) > max_width)
+        {
+            transform.position = new Vector3(max_width, transform.position.y, 0);
+        }
+
+
     }
+
 }
--- Shooter/Assets/Scripts/Spawning.cs
+++ Shooter/Assets/Scripts/Spawning.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class Spawning : MonoBehaviour
 {
+
+    public GameObject asteroid;
+    public GameObject container;
+
+    private int randomgen;
+    private int randomgen1;
+
+
+    private void SpawnObject()
+    {
+        randomgen = Random.Range(1, 3);
+
+        if (randomgen == 1)
+        {
+            randomgen1 = Random.Range(3, 5);
+
+            if (randomgen1 == 3)
+            {
+                GameObject asteroid_instance = Instantiate(asteroid, new Vector3(-8.40f, Random.Range(4.30f, -4.30f)), Quaternion.identity);
+
+                asteroid_instance.transform.SetParent(container.transform);
+            }
+
+            if (randomgen1 == 4)
+            {
+                GameObject asteroid_instance = Instantiate(asteroid, new Vector3(8.40f, Random.Range(4.30f, -4.30f)), Quaternion.identity);
+
+                asteroid_instance.transform.SetParent(container.transform);
+            }
+
+
+        }
+
+        if (randomgen == 2)
+        {
+            randomgen1 = Random.Range(3, 5);
+
+            if (randomgen1 == 3)
+            {
+                GameObject asteroid_instance = Instantiate(asteroid, new Vector3(Random.Range(8.40f, -8.40f), -4.3f), Quaternion.identity);
+
+                asteroid_instance.transform.SetParent(container.transform);
+            }
+
+            if (randomgen1 == 4)
+            {
+                GameObject asteroid_instance = Instantiate(asteroid, new Vector3(Random.Range(8.40f, -8.40f), 4.3f), Quaternion.identity);
+
+                asteroid_instance.transform.SetParent(container.transform);
+            }
+
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObject", 2, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+   
+
